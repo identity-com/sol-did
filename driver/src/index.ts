bde854fe304f9eb4457f67730f1731c5a80d2728
dankelleher <dankelleher@yahoo.com>
2021-03-06 08:33:10 +0100

    Support CORS requests in the driver

--- driver/src/index.ts
+++ driver/src/index.ts
@@ -38,6 +38,14 @@ const expressAppConfig = oas3Tools.expressAppConfig(
 
 const app = expressAppConfig.getApp();
 
+// enable cross-origin requests
+app.use((_req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
+});
+
 // Initialize the Swagger middleware
 export const server = http.createServer(app).listen(serverPort, () => {
   console.log(
