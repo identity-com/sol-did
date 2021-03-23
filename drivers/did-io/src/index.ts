ca0f0be4b7e99380a6e97c61cd062cafd0e1e98f
dankelleher <dankelleher@yahoo.com>
2021-03-23 12:29:04 +0100

    Add a did-io driver

---
+++ drivers/did-io/src/index.ts
@@ -0,0 +1,43 @@
+import {PublicKeyBase58, PrivateKey, ClusterType} from '@identity.com/solid-did-client';
+import * as DID from '@identity.com/solid-did-client';
+import {Cluster} from "@solana/web3.js";
+import {DIDDocument} from "did-resolver";
+
+type Properties = {
+  payer: PrivateKey
+}
+
+type GetParameters = {
+  did: string,
+}
+
+type RegisterParameters = {
+  key: PublicKeyBase58,
+  size: number,
+  cluster: 'localnet' | Cluster
+}
+
+export class Driver {
+  private payer: PrivateKey;
+
+  constructor({ payer }:Properties) {
+    this.payer = payer;
+  }
+
+  async get({did}:GetParameters):Promise<DIDDocument> {
+    return DID.resolve(did);
+  }
+
+  async register({key, size = 1000, cluster}:RegisterParameters) {
+    return DID.register({
+      owner: key,
+      payer: this.payer,
+      size,
+      cluster: ClusterType.parse(cluster)
+    })
+  }
+}
+
+const driver = (properties: Properties) => new Driver(properties);
+
+export default { driver };
---
+++ drivers/did-io/test/did-io.test.ts
@@ -0,0 +1,32 @@
+import didIo from 'did-io';
+import didKey from 'did-method-key'
+import didSolid from '../src/'
+import {Account, Connection} from "@solana/web3.js";
+import {ClusterType, SolanaUtil} from "@identity.com/solid-did-client";
+
+const cluster = ClusterType.devnet();
+
+describe('did-io integration', () => {
+  beforeAll(async () => {
+    const connection = new Connection(cluster.solanaUrl(), 'recent');
+    const payerAccount = await SolanaUtil.newAccountWithLamports(connection, 10000000);
+
+    didIo.use('key', didKey.driver());
+    didIo.use('solid', didSolid.driver({payer: payerAccount.secretKey}))
+  })
+
+  it('creates a did on devnet', async () => {
+    const owner = new Account();
+
+    const did = await didIo.register({
+      key: owner.publicKey.toBase58(),
+      didDocument: { did: 'did:solid:'},
+      cluster: cluster.toString()
+    })
+
+    const document = await didIo.get({ did });
+
+    expect(document.id).toEqual(did);
+    expect(document.verificationMethod[0].publicKeyBase58).toEqual(owner.publicKey.toBase58());
+  });
+});
