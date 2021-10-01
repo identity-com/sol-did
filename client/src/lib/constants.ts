804fc979ceef5e66924c80a75769c3171eb1cc7b
dankelleher <dankelleher@yahoo.com>
2021-10-01 16:41:05 +0200

    Switch the program ID for sol-did program v2

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -3,7 +3,7 @@ import { Commitment, PublicKey } from '@solana/web3.js';
 export const DID_HEADER = 'did';
 export const DID_METHOD = 'sol';
 export const PROGRAM_ID: PublicKey = new PublicKey(
-  'ide3Y2TubNMLLhiG1kDL6to4a8SjxD18YWCYC5BZqNV'
+  'idDa4XeCjVwKcprVAo812coUQbovSZ4kDGJf2sPaBnM'
 );
 export const SOLANA_COMMITMENT: Commitment = 'confirmed';
 export const DEFAULT_DOCUMENT_SIZE = 1_000;
