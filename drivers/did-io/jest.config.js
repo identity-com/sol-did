6e9ec45c7e9e697d127ed5e190e22839b7640ece
William Brooks <william@identity.org>
2022-01-12 11:21:30 +0200

    IDCOM-1427 Updated jest config for additional package transformations

--- drivers/did-io/jest.config.js
+++ drivers/did-io/jest.config.js
@@ -1,8 +1,19 @@
+const packagesToTransform = [
+  '@digitalbazaar/did-io',
+  '@digitalbazaar/lru-memoize',
+  '@digitalbazaar/ed25519-verification-key-2018',
+  '@digitalbazaar/did-io',
+  'base58-universal',
+].join('|')
+
 module.exports = {
   testEnvironment: 'node',
-  transformIgnorePatterns: ['/node_modules/(?!@digitalbazaar/(did-io|lru-memoize))'],
+  transformIgnorePatterns: [`/node_modules/(?!(${packagesToTransform}))`],
   moduleNameMapper: {
     '@digitalbazaar/did-io': '@digitalbazaar/did-io/lib/main.js',
     '@digitalbazaar/lru-memoize': '@digitalbazaar/lru-memoize/lib/main.js',
+    '@digitalbazaar/ed25519-verification-key-2018': '@digitalbazaar/ed25519-verification-key-2018/src/main.js',
+    '@digitalbazaar/did-io': '@digitalbazaar/did-io/lib/main.js',
+    'base58-universal': 'base58-universal/main.js',
   }
 };
