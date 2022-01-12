299ef50fed13fbcfbbe9a27a47cf366946b0d465
William Brooks <william@identity.org>
2022-01-12 11:34:12 +0200

    IDCOM-1427 Updated types.d.ts

--- drivers/did-io/src/types.d.ts
+++ drivers/did-io/src/types.d.ts
@@ -2,23 +2,4 @@ type Driver = {};
 
 declare module '@digitalbazaar/did-io';
 declare module '@digitalbazaar/did-method-key';
-
-declare module 'did-io' {
-  function use(methodName: string, driver: Driver): void;
-  function register(options: any): any;
-  function get(options: any): any;
-
-  export = {
-    use,
-    register,
-    get,
-  };
-}
-
-declare module 'did-method-key' {
-  function driver(): Driver;
-
-  export = {
-    driver,
-  };
-}
+declare module '@digitalbazaar/ed25519-verification-key-2018';
