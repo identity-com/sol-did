e2dfc5a17785f72ef28d2af7805ab45acb208978
William Brooks <william@identity.org>
2022-01-12 13:03:46 +0200

    IDCOM-1427 Updated TS types

--- drivers/did-io/src/types.d.ts
+++ drivers/did-io/src/types.d.ts
@@ -1,5 +1,25 @@
 type Driver = {};
 
-declare module '@digitalbazaar/did-io';
-declare module '@digitalbazaar/did-method-key';
-declare module '@digitalbazaar/ed25519-verification-key-2018';
+declare module '@digitalbazaar/did-io' {
+  class CachedResolver {
+    get(options: { did: string }): any;
+
+    generate(options: any): { didDocument: any, keyPairs: any, methodFor: Function };
+
+    use(driver: Driver): void;
+  }
+
+  function findVerificationMethod(options: { doc: any, purpose: string }): any;
+
+  return {
+    CachedResolver
+  }
+}
+
+declare module '@digitalbazaar/did-method-key' {
+  function driver(): Driver;
+
+  return {
+    driver
+  }
+}
