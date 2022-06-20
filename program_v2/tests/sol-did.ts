c1d4cd471eab8067cac1204093534549af7a7d9e
Martin Riedel <web@riedel-it.de>
2022-06-20 08:07:24 -0400

    feature: Initial anchor setup.

---
+++ program_v2/tests/sol-did.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { SolDid } from "../target/types/sol_did";
+
+describe("sol-did", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.SolDid as Program<SolDid>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
