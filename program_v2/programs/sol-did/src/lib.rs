0dce68b0988d0315ef14b50c72c86b9750830b2f
dankelleher <dankelleher@yahoo.com>
2022-08-13 07:30:00 +0200

    [WIP] Add top-level function to check if an address is an authority on the did

--- program_v2/programs/sol-did/src/lib.rs
+++ program_v2/programs/sol-did/src/lib.rs
@@ -9,12 +9,12 @@ mod utils;
 use anchor_lang::prelude::*;
 use instructions::*;
 use state::{Secp256k1RawSignature, Service, VerificationMethod};
+use crate::state::DidAccount;
 
 declare_id!("didso1Dpqpm4CsiCjzP766BGY89CAdD6ZBL68cRhFPc");
 
 #[program]
 pub mod sol_did {
-
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>, size: u32) -> Result<()> {
@@ -91,5 +91,75 @@ pub mod sol_did {
 
     pub fn migrate(ctx: Context<Migrate>) -> Result<()> {
         instructions::migrate(ctx)
+    }
+
+}
+
+pub fn is_authority(
+    did_account: &AccountInfo,
+    sol_authority: &Pubkey,
+    eth_message: &[u8],
+    eth_raw_signature: Option<&Secp256k1RawSignature>,
+    filter_fragment: Option<&String>,
+) -> Result<bool> {
+    let did_data : Account<DidAccount> = Account::try_from(did_account)?;
+    let authority_exists = did_data.find_authority(sol_authority, eth_message, eth_raw_signature, filter_fragment).is_some();
+
+    Ok(authority_exists)
+}
+
+#[cfg(test)]
+mod test {
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    use crate::state::DidAccount;
+    use super::*;
+
+    fn create_test_authority() -> Pubkey { Pubkey::new_unique() }
+
+    fn create_test_did(test_authority: Pubkey) -> DidAccount {
+        DidAccount {
+            version: 0,
+            bump: 0,
+            nonce: 0,
+            initial_verification_method: VerificationMethod {
+                fragment: "".to_string(),
+                flags: 0,
+                method_type: 0,
+                key_data: test_authority.to_bytes().to_vec()
+            },
+            verification_methods: vec![],
+            services: vec![],
+            native_controllers: vec![],
+            other_controllers: vec![]
+        }
+    }
+
+    #[test]
+    fn test_is_authority() {
+        let test_authority = create_test_authority();
+        let test_did_account = create_test_did(test_authority);
+        let mut data: Vec<u8> = AnchorSerialize::try_to_vec(&test_did_account).unwrap();
+
+        let mut lamports = 1; // must be > 0 to pass the Account::try_from
+        let account_info = AccountInfo {
+            key: &Default::default(),
+            is_signer: false,
+            is_writable: false,
+            lamports: Rc::new(RefCell::new(&mut lamports)),
+            data: Rc::new(RefCell::new(&mut data)),
+            owner: &id(),
+            executable: false,
+            rent_epoch: 0
+        };
+
+        let result = is_authority(
+            &account_info,
+            &test_authority,
+            &[],
+            None,
+            None
+        ).unwrap();
+        assert!(result);
     }
 }
