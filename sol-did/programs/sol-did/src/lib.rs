8d4e6c772bd0480995dffb436627d4021c523952
dankelleher <dankelleher@yahoo.com>
2022-08-13 07:30:00 +0200

    Add top-level function to check if an address is an authority on the did

--- sol-did/programs/sol-did/src/lib.rs
+++ sol-did/programs/sol-did/src/lib.rs
@@ -6,6 +6,7 @@ mod security_txt;
 mod state;
 mod utils;
 
+use crate::state::DidAccount;
 use anchor_lang::prelude::*;
 use instructions::*;
 use state::{Secp256k1RawSignature, Service, VerificationMethod};
@@ -14,7 +15,6 @@ declare_id!("didso1Dpqpm4CsiCjzP766BGY89CAdD6ZBL68cRhFPc");
 
 #[program]
 pub mod sol_did {
-
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>, size: u32) -> Result<()> {
@@ -91,5 +91,175 @@ pub mod sol_did {
 
     pub fn migrate(ctx: Context<Migrate>) -> Result<()> {
         instructions::migrate(ctx)
+    }
+}
+
+pub fn is_authority(
+    did_account: &AccountInfo,
+    sol_authority: &Pubkey,
+    eth_message: &[u8],
+    eth_raw_signature: Option<&Secp256k1RawSignature>,
+    filter_fragment: Option<&String>,
+) -> Result<bool> {
+    let did_data: Account<DidAccount> = Account::try_from(did_account)?;
+    let authority_exists = did_data
+        .find_authority(
+            sol_authority,
+            eth_message,
+            eth_raw_signature,
+            filter_fragment,
+        )
+        .is_some();
+
+    Ok(authority_exists)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::constants::VM_DEFAULT_FRAGMENT_NAME;
+    use crate::state::{DidAccount, VerificationMethodFlags};
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    fn create_test_authority() -> Pubkey {
+        Pubkey::new_unique()
+    }
+
+    fn create_test_did(test_authority: Pubkey) -> DidAccount {
+        DidAccount {
+            version: 0,
+            bump: 0,
+            nonce: 0,
+            initial_verification_method: VerificationMethod {
+                fragment: VM_DEFAULT_FRAGMENT_NAME.to_string(),
+                flags: VerificationMethodFlags::CAPABILITY_INVOCATION.bits(),
+                method_type: 0,
+                key_data: test_authority.to_bytes().to_vec(),
+            },
+            verification_methods: vec![],
+            services: vec![],
+            native_controllers: vec![],
+            other_controllers: vec![],
+        }
+    }
+
+    #[test]
+    fn test_is_authority() {
+        let test_authority = create_test_authority();
+        let test_did_account = create_test_did(test_authority);
+        let mut data: Vec<u8> = Vec::with_capacity(1024);
+        test_did_account.try_serialize(&mut data).unwrap();
+
+        let mut lamports = 1; // must be > 0 to pass the Account::try_from
+        let account_info = AccountInfo {
+            key: &Default::default(),
+            is_signer: false,
+            is_writable: false,
+            lamports: Rc::new(RefCell::new(&mut lamports)),
+            data: Rc::new(RefCell::new(&mut data)),
+            owner: &id(),
+            executable: false,
+            rent_epoch: 0,
+        };
+
+        let should_be_true = is_authority(&account_info, &test_authority, &[], None, None).unwrap();
+        assert!(should_be_true);
+    }
+
+    #[test]
+    fn test_is_authority_fails_if_key_is_not_on_did() {
+        let test_authority = create_test_authority();
+        let some_other_authority = create_test_authority();
+
+        let test_did_account = create_test_did(test_authority);
+        let mut data: Vec<u8> = Vec::with_capacity(1024);
+        test_did_account.try_serialize(&mut data).unwrap();
+
+        let mut lamports = 1; // must be > 0 to pass the Account::try_from
+        let account_info = AccountInfo {
+            key: &Default::default(),
+            is_signer: false,
+            is_writable: false,
+            lamports: Rc::new(RefCell::new(&mut lamports)),
+            data: Rc::new(RefCell::new(&mut data)),
+            owner: &id(),
+            executable: false,
+            rent_epoch: 0,
+        };
+
+        let should_be_false =
+            is_authority(&account_info, &some_other_authority, &[], None, None).unwrap();
+        assert!(!should_be_false);
+    }
+
+    #[test]
+    fn test_is_authority_passes_for_non_default_verification_method() {
+        let test_authority = create_test_authority();
+        let some_other_authority = create_test_authority();
+
+        let mut test_did_account = create_test_did(test_authority);
+        test_did_account
+            .verification_methods
+            .push(VerificationMethod {
+                fragment: "second_key".to_string(),
+                flags: VerificationMethodFlags::CAPABILITY_INVOCATION.bits(),
+                method_type: 0,
+                key_data: some_other_authority.to_bytes().to_vec(),
+            });
+
+        let mut data: Vec<u8> = Vec::with_capacity(1024);
+        test_did_account.try_serialize(&mut data).unwrap();
+
+        let mut lamports = 1; // must be > 0 to pass the Account::try_from
+        let account_info = AccountInfo {
+            key: &Default::default(),
+            is_signer: false,
+            is_writable: false,
+            lamports: Rc::new(RefCell::new(&mut lamports)),
+            data: Rc::new(RefCell::new(&mut data)),
+            owner: &id(),
+            executable: false,
+            rent_epoch: 0,
+        };
+
+        let should_be_true =
+            is_authority(&account_info, &some_other_authority, &[], None, None).unwrap();
+        assert!(should_be_true);
+    }
+
+    #[test]
+    fn test_is_authority_fails_if_the_key_is_not_capability_invocation() {
+        let test_authority = create_test_authority();
+        let some_other_authority = create_test_authority();
+
+        let mut test_did_account = create_test_did(test_authority);
+        test_did_account
+            .verification_methods
+            .push(VerificationMethod {
+                fragment: "second_key".to_string(),
+                flags: VerificationMethodFlags::AUTHENTICATION.bits(), // not CAPABILITY_INVOCATION
+                method_type: 0,
+                key_data: some_other_authority.to_bytes().to_vec(),
+            });
+
+        let mut data: Vec<u8> = Vec::with_capacity(1024);
+        test_did_account.try_serialize(&mut data).unwrap();
+
+        let mut lamports = 1; // must be > 0 to pass the Account::try_from
+        let account_info = AccountInfo {
+            key: &Default::default(),
+            is_signer: false,
+            is_writable: false,
+            lamports: Rc::new(RefCell::new(&mut lamports)),
+            data: Rc::new(RefCell::new(&mut data)),
+            owner: &id(),
+            executable: false,
+            rent_epoch: 0,
+        };
+
+        let should_be_false =
+            is_authority(&account_info, &some_other_authority, &[], None, None).unwrap();
+        assert!(!should_be_false);
     }
 }
