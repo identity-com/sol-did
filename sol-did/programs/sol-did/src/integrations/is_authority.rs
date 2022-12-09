64325e9ddbad631fb24721ce2906cf0b07f1f7fc
dankelleher <dankelleher@yahoo.com>
2022-12-09 19:05:12 +0100

    IDCOM-2012 Rust formatting

--- sol-did/programs/sol-did/src/integrations/is_authority.rs
+++ sol-did/programs/sol-did/src/integrations/is_authority.rs
@@ -369,7 +369,9 @@ mod test {
         let controller_did_account_address = derive_did_account(&controller_authority.to_bytes());
         let controlled_did_account_address = derive_did_account(&controller_authority.to_bytes());
 
-        controlled_did_account.set_native_controllers(vec![controller_authority]).unwrap();
+        controlled_did_account
+            .set_native_controllers(vec![controller_authority])
+            .unwrap();
 
         let mut controller_data: Vec<u8> = Vec::with_capacity(1024);
         controller_did_account
@@ -430,7 +432,9 @@ mod test {
         let controller_did_account_address = derive_did_account(&controller_authority.to_bytes());
         let controlled_did_account_address = derive_did_account(&controller_authority.to_bytes());
 
-        controlled_did_account.set_native_controllers(vec![controller_authority]).unwrap();
+        controlled_did_account
+            .set_native_controllers(vec![controller_authority])
+            .unwrap();
 
         let mut controller_data: Vec<u8> = Vec::with_capacity(0);
 
